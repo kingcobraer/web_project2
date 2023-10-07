24d1230caed381091c7e0e823186786b6a469616
kingcobraer <997818503@qq.com>
2023-10-07 18:25:02 +0800

    更新了table4

--- src/views/Table4.vue
+++ src/views/Table4.vue
@@ -1,7 +1,20 @@
 <template>
-  <el-table :data="students" style="width: 100%">
-    <el-table-column prop="name" label="姓名" width="180" />
-    <el-table-column prop="english" label="英语成绩" width="180" />
+  <el-table
+    :data="students"
+    style="width: 100%"
+    stripe
+    border
+    height="550"
+    :default-sort="{ prop: 'english', order: 'descending' }"
+  >
+    <!-- 可选参数: stripe 斑马底纹  border 边框  height="550" 整个表格的高度 均为可选设计参数 -->
+
+    <el-table-column fixed="left" prop="name" label="姓名" width="180" sortable />
+    <!--可选参数 固定在左侧或右侧: fixed="right" -->
+
+    <el-table-column prop="english" label="英语成绩" width="180" sortable />
+    <!-- 可选参数 排序: sortable 或者 sortable="", 不可以用sortable="true", 原因不明  -->
+
     <el-table-column prop="math" label="数学成绩" width="180" />
     <el-table-column prop="chinese" label="语文成绩" width="180" />
   </el-table>
